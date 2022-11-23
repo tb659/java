61c11faa233f6e8886ed68a947504e97661d694e
tb659 <tb541122@163.com>
2022-11-23 23:06:40 +0800

    新增删除选中servlet

---
+++ demo_09_case/src/web/servlet/DeleteSelectedServlet.java
@@ -0,0 +1,25 @@
+package web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Classname ${NAME}
+ * @Description TODO
+ * @Date 2022-11-23 23:02
+ * @Created by @tb659
+ */
+@WebServlet("/deleteSelectedServlet")
+public class DeleteSelectedServlet extends HttpServlet {
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+  }
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    this.doPost(request, response);
+  }
+}
--- demo_09_case/web/list.jsp
+++ demo_09_case/web/list.jsp
@@ -55,7 +55,7 @@
         <a class="btn btn-primary" href="javascript:void(0);" id="delSelected">删除选中</a>
       </div>
 
-      <form id="form" action="${pageContext.request.contextPath}/delSelectedServlet" method="post">
+      <form id="form" action="${pageContext.request.contextPath}/deleteSelectedServlet" method="post">
         <table border="1" class="table table-bordered table-hover">
           <tr class="success">
             <th><input type="checkbox" id="firstCb"></th>
