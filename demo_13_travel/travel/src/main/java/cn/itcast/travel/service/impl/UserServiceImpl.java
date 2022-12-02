f7e34979491856ca7a9996bb1d033dde0b87fb65
tb659 <tb541122@163.com>
2022-12-02 22:15:00 +0800

    发邮件

--- demo_13_travel/travel/src/main/java/cn/itcast/travel/service/impl/UserServiceImpl.java
+++ demo_13_travel/travel/src/main/java/cn/itcast/travel/service/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import cn.itcast.travel.dao.UserDao;
 import cn.itcast.travel.dao.impl.UserDaoImpl;
 import cn.itcast.travel.domain.User;
 import cn.itcast.travel.service.UserService;
+import cn.itcast.travel.util.MailUtils;
+import cn.itcast.travel.util.UuidUtil;
 
 /**
  * @Classname UserServiceImpl
@@ -29,7 +31,12 @@ public class UserServiceImpl implements UserService {
     if (u != null) {
       return false;
     }
+    user.setCode(UuidUtil.getUuid());
+    user.setStatus("N");
     UserDao.register(user);
+
+    String content = "<a href='http://localhost:8080/travel/activeUser?code="+ user.getCode() + "'>点击激活旅游网</a>";
+    MailUtils.sendMail(user.getEmail(), content, "激活邮件");
     return true;
   }
 }
